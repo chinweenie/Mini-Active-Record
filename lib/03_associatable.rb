ff1fca3fe96afd34c4d2f662c0aec838f40dbce6
Chin Wee Nie <chinweenie@gmail.com>
2019-12-18 17:02:55 +0800

    Add associatable

--- lib/03_associatable.rb
+++ lib/03_associatable.rb
@@ -3,48 +3,73 @@ require 'active_support/inflector'
 
 # Phase IIIa
 class AssocOptions
-  attr_accessor(
-    :foreign_key,
-    :class_name,
-    :primary_key
-  )
-
+  attr_accessor :foreign_key, :class_name, :primary_key
   def model_class
-    # ...
+    @class_name.constantize
   end
 
   def table_name
-    # ...
+    self.model_class.table_name
   end
 end
 
 class BelongsToOptions < AssocOptions
   def initialize(name, options = {})
-    # ...
+    defaults = {
+      :foreign_key => "#{name}_id".to_sym,
+      :class_name => name.to_s.camelcase,
+      :primary_key => :id
+    }
+
+    defaults.keys.each do |key|
+      self.send("#{key}=", options[key] || defaults[key])
+    end
   end
 end
 
+# shop has many products, foreign_key = shop_id, classname = Product, primary_key = id
 class HasManyOptions < AssocOptions
   def initialize(name, self_class_name, options = {})
-    # ...
+    defaults = {
+      :foreign_key => "#{self_class_name.underscore}_id".to_sym,
+      :class_name => name.to_s.singularize.camelcase,
+      :primary_key => :id
+    }
+
+    defaults.keys.each do |key|
+      self.send("#{key}=", options[key] || defaults[key])
+    end
   end
 end
 
 module Associatable
-  # Phase IIIb
   def belongs_to(name, options = {})
-    # ...
+    self.assoc_options[name] = BelongsToOptions.new(name, options)
+    define_method(name) do    
+      options = self.class.assoc_options[name]
+      key_val = self.send(options.foreign_key)
+      # where returns an array, we want the first element only
+      options.model_class.where(options.primary_key => key_val).first
+    end
   end
 
   def has_many(name, options = {})
-    # ...
+    self.assoc_options[name] = HasManyOptions.new(name, self.name, options)
+    define_method(name) do    
+      options = self.class.assoc_options[name]
+      key_val = self.send(options.primary_key)
+      # we don't need first since has many should return an array of objects belong to this model
+      options.model_class.where(options.foreign_key => key_val)
+    end
   end
 
   def assoc_options
     # Wait to implement this in Phase IVa. Modify `belongs_to`, too.
+    @assoc_options ||= {}
+    @assoc_options
   end
 end
 
 class SQLObject
-  # Mixin Associatable here...
+  extend Associatable
 end
